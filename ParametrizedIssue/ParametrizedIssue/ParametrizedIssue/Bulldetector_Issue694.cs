95b8167c555385b6465ec0aa2ff181f46972f9fd
Terje Sandstrom <terje.sandstrom.personal@outlook.com>
2020-01-11 22:12:52 +0100

    Added tests for Issue 694 and some misc tests too

---
+++ ParametrizedIssue/ParametrizedIssue/ParametrizedIssue/Bulldetector_Issue694.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+
+namespace ParametrizedIssue
+{
+    public class Bulldetector_Issue694
+    {
+
+        private static readonly List<string> RandomStrings = new List<string>
+        {
+            "abc", "def", "ghi"
+        };
+
+        [Test]
+        public void Is_applicable_when_no_constraints_are_set(
+            [ValueSource(nameof(RandomStrings))] string vehicleGroup,
+            [ValueSource(nameof(RandomStrings))] string fuelType,
+            [ValueSource(nameof(RandomStrings))] string carBody,
+            [Random(3)] int grossWeight)
+        {
+            // removed - not relevant
+        }
+    }
+}
---
+++ ParametrizedIssue/ParametrizedIssue/ParametrizedIssue/MiscTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace ParametrizedIssue
+{
+    public class MiscTests
+    {
+        [TestCase("aaa")]
+        [TestCase("bbb")]
+        [TestCase("ccc")]
+        public void TestCasesWithSimpleStrings(string s)
+        {
+
+        }
+
+        //[Test]
+        //public void TestCasesWithRandom([Random(3)] int someRandomInt)
+        //{
+
+        //}
+    }
+}
