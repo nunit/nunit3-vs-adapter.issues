4ea99504d40808d3740c2d7c0b15b399128c88a4
Terje Sandstrøm <terje@hermit.no>
2020-06-11 22:05:30 +0200

    Issue658

---
+++ Issue658/Explicit/Explicit/TestClass.cs
@@ -0,0 +1,29 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Explicit
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Explicit]
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            // TODO: Add your test code here
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+    }
+}
---
+++ Issue658/Explicit/ExplicitCore/UnitTest1.cs
@@ -0,0 +1,21 @@
+// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ExplicitCore
+{
+    [TestFixture]
+    public class TestClassCore
+    {
+        [Explicit]
+        [Test]
+        public void TestMethodCore()
+        {
+            // TODO: Add your test code here
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+    }
+}
