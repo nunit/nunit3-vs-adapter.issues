bf530aed0fd5329c45659fbe374ad7fa2a356922
Terje Sandstrøm <terje@hermit.no>
2020-06-11 22:06:22 +0200

    640 and 760

--- Issue760/UnitTest1.cs
+++ Issue760/UnitTest1.cs
@@ -20,5 +20,11 @@ namespace Issue760
         {
            // Assert.Fail();
         }
+
+        [TestCase(2, 3, 5, TestName = "Whatever it is")]
+        public void Test744(int a, int b, int c)
+        {
+
+        }
     }
 }
