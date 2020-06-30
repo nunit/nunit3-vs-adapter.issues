be4e367c83ae0120c8d80e4c2847c1336645e39b
Terje Sandstrøm <terje@hermit.no>
2020-06-30 23:47:34 +0200

    Added issue 545

---
+++ Issue545/Issue545/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Issue545
+{
+    [TestFixture]
+    public class FooTests
+    {
+        private static readonly TestCaseData[] Values =
+        {
+            new TestCaseData(1),
+            new TestCaseData(2),
+        };
+
+        [Explicit("Don't run this")]
+        [TestCaseSource(nameof(Values))]
+        public void Test1(object value)
+        {
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
