99583b5ec3f2c0ed25eb8a4b3939fa717e18d76d
Terje Sandstrøm <terje@hermit.no>
2020-06-30 23:47:15 +0200

    Added issue 497

---
+++ Issue497/ANonGeneratedExplicitTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Filtering
+{
+    public class ANonGeneratedExplicitTest
+    {
+        [Explicit]
+        [Test]
+        public void TestExplicitTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestNotExplicitTest()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
---
+++ Issue497/GeneratedTest94.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+public class GeneratedTest94
+{
+    [Test] public void Test1() { }
+    [Test] public void Test2() { }
+    [Test] public void Test3() { }
+    [Test] public void Test4() { }
+    [Test] public void Test5() { }
+    [Test] public void Test6() { }
+    [Test] public void Test7() { }
+    [Test] public void Test8() { }
+    [Test] public void Test9() { }
+    [Test] public void Test10() { }
+    [Test] public void Test11() { }
+    [Test] public void Test12() { }
+    [Test] public void Test13() { }
+    [Test] public void Test14() { }
+    [Test] public void Test15() { }
+    [Test] public void Test16() { }
+    [Test] public void Test17() { }
+    [Test] public void Test18() { }
+    [Test] public void Test19() { }
+    [Test] public void Test20() { }
+    [Test] public void Test21() { }
+    [Test] public void Test22() { }
+    [Test] public void Test23() { }
+    [Test] public void Test24() { }
+    [Test] public void Test25() { }
+    [Test] public void Test26() { }
+    [Test] public void Test27() { }
+    [Test] public void Test28() { }
+    [Test] public void Test29() { }
+    [Test] public void Test30() { }
+    [Test] public void Test31() { }
+    [Test] public void Test32() { }
+    [Test] public void Test33() { }
+    [Test] public void Test34() { }
+    [Test] public void Test35() { }
+    [Test] public void Test36() { }
+    [Test] public void Test37() { }
+    [Test] public void Test38() { }
+    [Test] public void Test39() { }
+    [Test] public void Test40() { }
+}
