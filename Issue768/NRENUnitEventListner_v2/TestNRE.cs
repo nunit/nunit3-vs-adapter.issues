cbd45225e7973142f079ece90e951bc3b7abe599
Ace Olszowka <aceo@cu.net>
2020-07-13 13:41:45 -0600

    Added Repro For nunit3-vs-adapter/issues/768

---
+++ Issue768/NRENUnitEventListner_v2/TestNRE.cs
@@ -0,0 +1,14 @@
+﻿namespace NRENUnitEventListner_v2
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestNRE
+    {
+        [TestCase]
+        public void SimpleTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
