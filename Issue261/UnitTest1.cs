ff9674469c602b151b21f1319413c07b747367eb
Terje Sandstrøm <terje@hermit.no>
2020-08-18 19:57:38 +0200

    Addin repro for 261

---
+++ Issue261/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace UnitTestProject1
+{
+  [TestFixture]
+  public class UnitTest1
+  {
+    [Test]
+    public void TestMethod1()
+    {
+      Assert.AreEqual(1,1);
+    }
+  }
+}
