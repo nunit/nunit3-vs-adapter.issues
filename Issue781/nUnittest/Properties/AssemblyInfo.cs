e04643bf0b7e0a86848f4304119bb57c04c9a95f
Terje Sandstrøm <terje@hermit.no>
2020-09-06 14:40:58 +0200

    Issue781

---
+++ Issue781/nUnittest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("nUnittest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("nUnittest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dc569425-6995-4608-81a6-c6e0f9033ac9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Issue781/nUnittest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace nUnittest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [OneTimeSetUp]
+        public static void OneTimeSetUp()
+        {
+            Console.WriteLine("OneTimeSetUp");
+        }
+        [SetUp]
+        public void SetUp()
+        {
+            Console.WriteLine("SetUp");
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            Console.WriteLine("TearDown");
+        }
+        [Test]
+        public void Test()
+        {
+            Console.WriteLine("Test");
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        {
+            Console.WriteLine("OneTimeTearDown");
+        }
+    }
+}
