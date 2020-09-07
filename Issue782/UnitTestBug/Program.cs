b89820b8b23dfae49d400dbea63b5f976687542b
Terje Sandstrøm <terje@hermit.no>
2020-09-07 17:25:11 +0200

    Adding Issue782

---
+++ Issue782/UnitTestBug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTestBug
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ Issue782/UnitTestBugTests/ProgramTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UnitTestBug.Tests
+{
+	[TestFixture]
+	public class ProgramTests
+	{
+		[TestCaseSource(typeof(TupleTestsCases))]
+		public void TupleTests(IEnumerable<(object, object)> args)
+		{
+			Assert.Fail();
+		}
+
+		private class TupleTestsCases : IEnumerable
+		{
+			public IEnumerator GetEnumerator()
+			{
+				yield return new TestCaseData(new[] { (new object(), new object()) });
+			}
+		}
+	}
+}
