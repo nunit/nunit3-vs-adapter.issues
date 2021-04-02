a8e0bc2f5f417ba36154eca93f64fcd725158b0d
Terje Sandstrøm <terje@hermit.no>
2021-04-02 20:23:57 +0200

    issue811

---
+++ Issue811/ExplicitIssue.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace NUnit3VSIssue811
+{
+    [TestFixture]
+    [Explicit("https://github.com/nunit/nunit3-vs-adapter/issues/811")]
+    public class Explicit
+    {
+        [Test]
+        public void ExplicitTest()
+        {
+        }
+    }
+}
---
+++ Issue811/IgnoreIssue.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace NUnit3VSIssue811
+{
+    [TestFixture]
+    [Ignore("https://github.com/nunit/nunit3-vs-adapter/issues/811")]
+    public class Ignore
+    {
+        [Test]
+        public void IgnoredTest()
+        {
+        }
+    }
+}
