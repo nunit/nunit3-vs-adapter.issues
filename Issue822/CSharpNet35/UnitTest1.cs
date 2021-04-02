7231f27745659ea1a4b5e7639b44fdbdd4087d5c
Terje Sandstrøm <terje@hermit.no>
2021-04-02 21:38:50 +0200

    Issue 822

---
+++ Issue822/CSharpNet35/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CSharpNet35
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
