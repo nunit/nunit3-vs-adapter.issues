aafcbbf1d451d0f73b0c95f053d67707c32f5f6d
Terje Sandstrøm <terje@hermit.no>
2021-04-17 22:26:25 +0200

    Explaining the random case

--- done/Issue676/GetPlayerBalancesTests.cs
+++ done/Issue676/GetPlayerBalancesTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace NUnitTest
@@ -14,14 +13,56 @@ namespace NUnitTest
 
         protected const string GameToken = "c38cb9ef-27ef-e911-80e1-000d3ab693ab";
 
+        protected const string ValidToken = "abc";
+
         protected static string CorrelationToken => Guid.NewGuid().ToString();
 
-        [Test]
+        protected static Guid BaseToken = new Guid(GameToken); 
+
+        protected static string ModifiedCorrelationToken
+        {
+            get
+            {
+                BaseToken = BaseToken.NextGuid();
+                return BaseToken.ToString();
+            }
+        } 
+
+        private static string startString = "abc";
+        private static int next = 'd';
+        private static int i = 1;
+
+        protected static string WhateverToken
+        {
+            get
+            {
+                startString += (char)next;
+                startString += i;
+                next++;
+                i++;
+                return startString;
+            }
+        }
+
+
         [TestCaseSource(nameof(CreateInvalidPlayerBalancesRequests))]
         public void PlayerBalancesTest(PlayerBalancesRequest request, string siteCode, string correlationToken)
         {
             Assert.Pass();
         }
+
+        [TestCaseSource(nameof(CreateValidPlayerBalancesRequests))]
+        public void ValidPlayerBalancesTest(PlayerBalancesRequest request, string siteCode, string correlationToken)
+        {
+            Assert.Pass();
+        }
+
+        [TestCaseSource(nameof(CreateModifiedPlayerBalancesRequests))]
+        public void ModifiedPlayerBalancesTest(PlayerBalancesRequest request, string siteCode, string correlationToken)
+        {
+            Assert.Pass();
+        }
+
 
         private static object[] CreateInvalidPlayerBalancesRequests()
         {
@@ -36,6 +77,42 @@ namespace NUnitTest
                 new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, null, CorrelationToken },
                 new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, string.Empty, CorrelationToken },
                 new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, new string('*', 51), CorrelationToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, SiteCode, null },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, SiteCode, string.Empty },
+            };
+        }
+
+        private static object[] CreateValidPlayerBalancesRequests()
+        {
+            return new object[]
+            {
+                new object[] { new PlayerBalancesRequest(), null, null },
+                new object[] { new PlayerBalancesRequest { PlayerId = -1, Token = ValidToken, CurrencyCode = CurrencyCode }, SiteCode, WhateverToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = 0, Token = ValidToken, CurrencyCode = CurrencyCode }, SiteCode, WhateverToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = null, CurrencyCode = CurrencyCode }, SiteCode, WhateverToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = string.Empty, CurrencyCode = CurrencyCode }, SiteCode, WhateverToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = new string('*', 51), CurrencyCode = CurrencyCode }, SiteCode, WhateverToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, null, WhateverToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, string.Empty, WhateverToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, new string('*', 51), WhateverToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, SiteCode, null },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, SiteCode, string.Empty },
+            };
+        }
+
+        private static object[] CreateModifiedPlayerBalancesRequests()
+        {
+            return new object[]
+            {
+                new object[] { new PlayerBalancesRequest(), null, null },
+                new object[] { new PlayerBalancesRequest { PlayerId = -1, Token = GameToken, CurrencyCode = CurrencyCode }, SiteCode, ModifiedCorrelationToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = 0, Token = GameToken, CurrencyCode = CurrencyCode }, SiteCode, ModifiedCorrelationToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = null, CurrencyCode = CurrencyCode }, SiteCode, ModifiedCorrelationToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = string.Empty, CurrencyCode = CurrencyCode }, SiteCode, ModifiedCorrelationToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = new string('*', 51), CurrencyCode = CurrencyCode }, SiteCode, ModifiedCorrelationToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, null, ModifiedCorrelationToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, string.Empty, ModifiedCorrelationToken },
+                new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, new string('*', 51), ModifiedCorrelationToken },
                 new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, SiteCode, null },
                 new object[] { new PlayerBalancesRequest { PlayerId = PlayerId, Token = GameToken, CurrencyCode = CurrencyCode }, SiteCode, string.Empty },
             };
---
+++ done/Issue676/GuidExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NUnitTest
+{
+    // From https://stackoverflow.com/questions/30404965/increment-guid-in-c-sharp
+    internal static class GuidExtensions
+    {
+
+        static int[] byteOrder = {15, 14, 13, 12, 11, 10, 9, 8, 6, 7, 4, 5, 0, 1, 2, 3};
+
+        internal static Guid NextGuid(this Guid guid)
+        {
+            var bytes = guid.ToByteArray();
+            var canIncrement = byteOrder.Any(i => ++bytes[i] != 0);
+            return new Guid(canIncrement ? bytes : new byte[16]);
+        }
+    }
+}
