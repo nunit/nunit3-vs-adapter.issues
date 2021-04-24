10fd612c92c139b871876ded39554a22bff2f8ce
Terje Sandstrøm <terje@hermit.no>
2021-04-24 16:34:22 +0200

    Issue 846

--- Issue846/Issue846/UnitTest1.cs
+++ Issue846/Issue846/UnitTest1.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
 using NUnit.Framework;
 
 namespace Issue846
@@ -13,6 +18,26 @@ namespace Issue846
         public void Test1()
         {
             Assert.Pass();
+        }
+
+        [TestCase("sv-SE")]
+        [TestCase("nb-NO")]
+        [TestCase("nn-NO")]
+        [TestCase("da-DK")]
+        public void TestCulture(string culture)
+        {
+            var input = new List<string>
+            {
+                "XXX","AAA", "BBB", "AAB", "ABA"
+            };
+            var cult = new CultureInfo(culture);
+            var result = input.OrderBy(f => f, StringComparer.Create(cult, true)).ToList();
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.First(), Is.EqualTo("AAA"));
+                Assert.That(result, Is.Not.EqualTo(input).AsCollection);
+            });
+            
         }
     }
 }
