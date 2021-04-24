976d4e2423b4876216992049768cde0a28d64834
Terje Sandstrøm <terje@hermit.no>
2021-04-24 17:52:38 +0200

    issue is that norwegian/daniush regard aa = å, so sorting differs for those cultures

--- Issue846/Issue846/UnitTest1.cs
+++ Issue846/Issue846/UnitTest1.cs
@@ -26,16 +26,21 @@ namespace Issue846
         [TestCase("da-DK")]
         public void TestCulture(string culture)
         {
+
+            var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures).Select(o=>o.Name).ToList();
+            Assert.That(cultures, Does.Contain(culture),$"Specified culture {culture} doesn't exist");
             var input = new List<string>
             {
                 "XXX","AAA", "BBB", "AAB", "ABA"
             };
             var cult = new CultureInfo(culture);
-            var result = input.OrderBy(f => f, StringComparer.Create(cult, true)).ToList();
+            // var result = input.OrderBy(f => f, StringComparer.Create(cult, true)).ToList();
+            var result = input.OrderBy(f => f, StringComparer.InvariantCultureIgnoreCase).ToList();
             Assert.Multiple(() =>
             {
                 Assert.That(result.First(), Is.EqualTo("AAA"));
-                Assert.That(result, Is.Not.EqualTo(input).AsCollection);
+                Assert.That(result.Last(), Is.EqualTo("XXX"));
+                Assert.That(result, Is.Not.EqualTo(input).AsCollection,"Has not been sorted at all");
             });
             
         }
