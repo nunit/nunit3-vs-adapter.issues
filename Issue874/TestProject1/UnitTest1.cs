a1b2e7493d20f2cf37058c659b549bf566785f5c
Terje Sandstrom <terje@hermit.no>
2021-11-07 01:05:51 +0100

    updated 874

--- Issue874/TestProject1/UnitTest1.cs
+++ Issue874/TestProject1/UnitTest1.cs
@@ -4,6 +4,7 @@ namespace TestProject1
 {
 	public class Tests
 	{
+		[Category("A")]
 		[Test]
 		[Explicit]
 		public void Test1()
@@ -11,8 +12,15 @@ namespace TestProject1
 			Assert.Fail();
 		}
 
+		[Category("A")]
         [Test]
         public void Test2()
+        {
+            Assert.Pass();
+        }
+
+		 [Test, Category("B")]
+        public void Test3()
         {
             Assert.Pass();
         }
