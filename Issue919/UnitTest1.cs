78ebe3f31bd51cbedaf3d19840a38449ec43dbb9
Terje Sandstrom <terje@hermit.no>
2021-11-28 22:30:24 +0100

    Issues 914 and 919

---
+++ Issue919/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace Issue919;
+
+ public class Tests
+ {
+    public class Foo
+   {
+       [TestCase(1)]
+       public void Baz(int a)
+       {
+           Assert.Pass();
+       }
+   }
+}
