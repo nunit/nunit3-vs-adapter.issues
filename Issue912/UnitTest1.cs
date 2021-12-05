61d7ba435cc7f15ad685c1e8bb4be6b5103d0a32
Terje Sandstrom <terje@hermit.no>
2021-12-05 15:29:31 +0100

    912, 918 and 919

---
+++ Issue912/UnitTest1.cs
@@ -0,0 +1,33 @@
+
+using NUnit.Framework;
+
+namespace Issue912
+{
+
+
+public class TestExplicit
+{
+
+    [Test]
+    [Explicit]
+    public void Test1()
+    {
+        Assert.Fail();
+    }
+
+    [Test]
+    [Category("FOO")]
+    public void TestFoo()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    [Category("BAR")]
+    public void TestBar()
+    {
+        Assert.Pass();
+    }
+
+}
+}
