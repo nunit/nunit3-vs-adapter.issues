c1ff5a9b02f86240a55c35f8b64878057e6e631c
Terje Sandstrom <terje@hermit.no>
2022-01-16 19:37:15 +0100

    update941

---
+++ Issue941/filtertest2/UnitTest1.cs
@@ -0,0 +1,32 @@
+namespace filtertest;
+
+public class Tests
+{
+        [Test]
+    public void Test21()
+    {
+        Assert.Pass();
+    }
+}
+
+public class Tests22
+                    {
+        
+                        [Test,Category("FooGroup"),Category("AllGroup")]
+                        public void Foo()
+                        {
+                            Assert.Pass();
+                        }
+
+                        [Test,Explicit,Category("IsExplicit"),Category("FooGroup"),Category("AllGroup")]
+                        public void FooExplicit()
+                        {
+                            Assert.Pass();
+                        }
+
+                        [Test, Category("BarGroup"),Category("AllGroup")]
+                        public void Bar()
+                        {
+                            Assert.Pass();
+                        }
+                    }
