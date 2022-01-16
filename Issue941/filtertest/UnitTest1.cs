4f59ff13e2f8ae3a3747d01789bbe2e836abeefa
Terje Sandstrom <terje@hermit.no>
2022-01-16 19:50:42 +0100

    update

--- Issue941/filtertest/UnitTest1.cs
+++ Issue941/filtertest/UnitTest1.cs
@@ -5,6 +5,8 @@ public class Tests
         [Test]
     public void Test1()
     {
+        TestContext.WriteLine($"Tests1 running");
+        
         Assert.Pass();
     }
 }
@@ -15,18 +17,21 @@ public class Tests2
                         [Test,Category("FooGroup"),Category("AllGroup")]
                         public void Foo()
                         {
+                            TestContext.WriteLine($"{nameof(Tests2.Foo)} running");
                             Assert.Pass();
                         }
 
                         [Test,Explicit,Category("IsExplicit"),Category("FooGroup"),Category("AllGroup")]
                         public void FooExplicit()
                         {
+                            TestContext.WriteLine($"{nameof(Tests2.FooExplicit)} running");
                             Assert.Pass();
                         }
 
                         [Test, Category("BarGroup"),Category("AllGroup")]
                         public void Bar()
                         {
+                            TestContext.WriteLine($"{nameof(Tests2.Bar)} running");
                             Assert.Pass();
                         }
                     }
--- Issue941/filtertest2/UnitTest1.cs
+++ Issue941/filtertest2/UnitTest1.cs
@@ -5,6 +5,7 @@ public class Tests
         [Test]
     public void Test21()
     {
+        TestContext.WriteLine($"Test21 running");
         Assert.Pass();
     }
 }
@@ -15,18 +16,21 @@ public class Tests22
                         [Test,Category("FooGroup"),Category("AllGroup")]
                         public void Foo()
                         {
+                            TestContext.WriteLine($"{nameof(Tests22.Foo)} running");
                             Assert.Pass();
                         }
 
                         [Test,Explicit,Category("IsExplicit"),Category("FooGroup"),Category("AllGroup")]
                         public void FooExplicit()
                         {
+                            TestContext.WriteLine($"{nameof(Tests22.FooExplicit)} running");
                             Assert.Pass();
                         }
 
                         [Test, Category("BarGroup"),Category("AllGroup")]
                         public void Bar()
                         {
+                            TestContext.WriteLine($"{nameof(Tests22.Bar)} running");
                             Assert.Pass();
                         }
                     }
