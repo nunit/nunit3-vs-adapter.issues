5e51d7950b47a0f94d2547bc341d417fc992b25a
Terje Sandstrom <terje@hermit.no>
2023-03-18 22:12:02 +0100

    updated with ways to fix

--- Issue1044/UnitTest1.cs
+++ Issue1044/UnitTest1.cs
@@ -2,12 +2,25 @@ namespace Issue1041;
 
 class Tests
   {
+
+      /// <summary>
+      ///  To ensure our filters are actually working, we dont want this test out
+      /// </summary>
+      [Test]
+      public void SomeOtherTest()
+      {
+        TestContext.WriteLine("This is some other test.");
+        Assert.Pass();
+      }
+
+
     [TestCaseSource(nameof(GetTestCases))]
     public void RegularTest()
     { 
         TestContext.WriteLine("This is a regular test.");
     }
 
+    [Category("ExplicitTests")]
     [TestCaseSource(nameof(GetTestCases))]
     [Explicit]
     public void ExplicitTest()
@@ -23,4 +36,13 @@ class Tests
         new TestCaseData() { TestName = $"The test number 2." }
       };
     }
-  }
+
+    static List<TestCaseData> GetTestCasesNormal()
+    {
+        return new List<TestCaseData>
+        {
+            new TestCaseData(),
+            new TestCaseData() 
+        };
+    }
+}
