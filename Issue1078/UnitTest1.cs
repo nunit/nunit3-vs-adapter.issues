b0dbc11bd90396c3591d2aa8d8c0ec7a1af4c6ac
Terje Sandstrom <terje@hermit.no>
2023-03-18 11:42:40 +0100

    Issue1078

---
+++ Issue1078/UnitTest1.cs
@@ -0,0 +1,37 @@
+namespace exp;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        Assert.Pass();
+    }
+
+    [Explicit]
+    [Test]
+    public void TestEx1()
+    {
+        Assert.Pass();
+    }
+
+    
+
+}
