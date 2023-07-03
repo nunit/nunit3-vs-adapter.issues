6afd02d2bc9b18db91d320dbc30391ee2224cc0c
Terje Sandstrom (AAD) <terje@hermit.no>
2023-07-03 18:53:43 +0200

    Issue1096

---
+++ Issue1096/S0/Issue.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace NUnitParseIssue.S0
+{
+    internal class Issue
+    {
+
+        [Test]
+        public void ThisIsAnIssue()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
