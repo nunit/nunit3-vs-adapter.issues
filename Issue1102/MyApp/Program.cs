f29a0605d50c972bb4f73eeffc8c3da372d0db16
Reinhard Saller <ravigneaux@outlook.com>
2023-07-03 20:46:27 +0200

    Add repro for issue #1102.

---
+++ Issue1102/MyApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.Error.WriteLine("Hello, error stream!");
+        }
+    }
+}
