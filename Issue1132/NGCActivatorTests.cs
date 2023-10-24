15f9b9b3553511e272fafac2a8d51f288699d066
Terje Sandstrom <terje@hermit.no>
2023-10-24 16:46:34 +0200

    Repro Issue 1132

---
+++ Issue1132/NGCActivatorTests.cs
@@ -0,0 +1,102 @@
+﻿using Autofac;
+using FakeItEasy;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using NUnitLite;
+
+namespace TestCase
+{
+    public interface INGCInstance
+    { 
+
+    }
+
+    public class TestComponent : INGCInstance
+    {
+        public string ComponentId => nameof(ComponentId);
+
+        public string CompName => nameof(CompName);
+
+        public void Start()
+        {
+        }
+    }
+
+    public class TestComponentA : TestComponent
+    { }
+
+    public class TestComponentB : TestComponent
+    { }
+
+
+
+    public class TestModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType(typeof(NGCActivatorTests));
+        }
+    }
+
+    internal static class Program
+    {
+        public static int Main(string[] args)
+        {
+            return new AutoRun().Execute(args);
+        }
+    }
+
+
+    [TestFixture]
+    public class NGCActivatorTests
+    {
+        public NGCActivatorTests()
+        {
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [TestCase]
+        public void Test_ModuleRegistration()
+        {
+            Assert.IsTrue(true); 
+        }
+
+        [TestCase]
+        public void Test_ComponentRegistration_Default()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestCase]
+        public void Test_ComponentRegistration_ConfigurationA()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestCase]
+        public void Test_ComponentRegistration_ConfigurationB()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
