2af746b3399ca9452ae7d7fb51148c8c0b0eaf40
Reinhard Saller <ravigneaux@outlook.com>
2024-05-02 21:14:57 +0200

    Implement ConsoleRecorder (proof of concept).

---
+++ Issue1167/ConsoleRecorder.PoC/IRecorder.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleRecorder.PoC
+{
+    /// <summary>
+    /// Provides mechanisms for recording data.
+    /// </summary>
+    public interface IRecorder
+    {
+        /// <summary>
+        /// Gets a value indicating whether a recording is running.
+        /// </summary>
+        bool IsRecording { get; }
+
+        /// <summary>
+        /// Starts the recording.
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Stops the recording.
+        /// </summary>
+        void Stop();
+
+        /// <summary>
+        /// Erases the recorded data and prepares the recorder for a new recording.
+        /// </summary>
+        void Reset();
+    }
+}
