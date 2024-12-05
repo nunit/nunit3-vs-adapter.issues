d9f5630726948d98bcf19612ae78b6a22438033a
Terje Sandstrom <terje@hermit.no>
2024-12-05 11:21:59 +0100

    Issue1225

---
+++ Issue1225/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework.Interfaces;
+
+namespace Issue1225;
+
+public class MyFixture
+{
+    private int i = 0;
+
+    [SetUp]
+    public void InitTest()
+    {
+        i++;
+        TestContext.Out.WriteLine(i);
+
+    }
+
+    [Test, Order(42)]
+    public void TestA_42()
+    {
+
+    }
+
+
+    [Test, Order(13)]
+    public void TestB_13() { /* ... */ }
+
+    [Test, Order(5)]
+    public void TestC_5()
+    {
+    }
+}
