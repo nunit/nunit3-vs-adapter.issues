e78bad21e2bd598b8cd5c958a48932ebefc86f4b
Terje Sandstrom <terje@hermit.no>
2025-01-23 20:14:07 +0100

    Added Issue1231

---
+++ Issue1231/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace NUnitTestNamespace;
+
+public class TestClass
+{
+    [Property("Priority","1")]
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Property("Whatever", "SomeValue")]
+    [Test]
+    public void Test2()
+    {
+        Assert.Pass();
+    }
+
+    [Category("SomeCategory")]
+    [Test]
+    public void Test3()
+    {
+        Assert.Pass();
+    }
+}
+
+public class TestClass2
+{
+    [Property("Priority","1")]
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
