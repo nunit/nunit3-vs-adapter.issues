f78efec038ebeacf7f219196d2d51a97ba70593b
Terje Sandstrom <terje@hermit.no>
2025-02-15 15:02:06 +0100

    Issue 1240

---
+++ Issue1240/TestProject1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+namespace TestProject1;
+
+public  class TestProgram
+{
+    [Test]
+    public static void Test1()
+    {
+        // ensure the execution is long enough to see it in Task manager
+        Thread.Sleep(5000);
+    }
+}
