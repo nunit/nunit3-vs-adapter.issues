244d7798920c999e49492654bfadab7a6219072f
Terje Sandstrom <terje@hermit.no>
2025-04-02 15:20:58 +0200

    Issue1254

---
+++ Issue1254/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Test.NS;
+
+public class NUnitTest
+{
+    [TestCase("StringLiteral", true, TestName = $"Test_StringLiteral_True")]
+    public void Test(string stringValue, bool boolValue)
+    {
+        Assert.That(stringValue, Is.Not.Null);
+        Assert.That(boolValue, Is.True);
+    }
+}
