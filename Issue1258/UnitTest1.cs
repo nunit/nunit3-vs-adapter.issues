1275cc012edb0ffe9b50734d638bd88f875142b2
Terje Sandstrom <terje@hermit.no>
2025-04-10 11:48:45 +0200

    Issue1258

---
+++ Issue1258/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿namespace NUnitMTP
+{
+    public class Tests
+    {
+        [Test, Category("Cat1")]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test, Category("Cat2")]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+    }
+}
