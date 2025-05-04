6fd59f1d9f931d4ee4c706ad1f768a3c6d78cba2
Terje Sandstrom <terje@hermit.no>
2025-05-04 21:54:34 +0200

    Issue1264

---
+++ Issue1264/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿namespace Issue1264;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test2()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test3()
+    {
+        Assert.Pass();
+    }
+}
