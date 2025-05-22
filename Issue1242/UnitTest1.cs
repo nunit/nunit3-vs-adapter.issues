fa362037ca9132b603de780dffcf58cad192d97d
Terje Sandstrom <terje@hermit.no>
2025-05-22 13:01:48 +0200

    Updated 1261 and 1186, added 1242 and 1243

---
+++ Issue1242/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿namespace Issue1242;
+
+public class MyItem
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+}
+
+public static class TestData
+{
+    public static IEnumerable<TestFixtureData> Data
+    {
+        get
+        {
+            yield return new TestFixtureData(new MyItem { Id = Guid.NewGuid(), Name = "my name" });
+            yield return new TestFixtureData(new MyItem { Id = Guid.NewGuid(), Name = "my other name" });
+            yield return new TestFixtureData(new MyItem { Id = Guid.NewGuid(), Name = "my old name" });
+
+            yield return new TestFixtureData(1, new MyItem { Id = Guid.NewGuid(), Name = "my name" });
+            yield return new TestFixtureData(2, new MyItem { Id = Guid.NewGuid(), Name = "my other name" });
+            yield return new TestFixtureData(3, new MyItem { Id = Guid.NewGuid(), Name = "my old name" });
+        }
+    }
+}
+
+[TestFixtureSource(typeof(TestData), nameof(TestData.Data))]
+public class Tests
+{
+    private readonly MyItem _item;
+
+    public Tests(MyItem item)
+    {
+        _item = item;
+    }
+
+    public Tests(int _, MyItem item)
+    {
+        _item = item;
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ Issue1243/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace Issue1243;
+
+public class Tests
+{
+    [Test]
+public void TestConsoleOutput()
+{
+    string version = "1.2.3";
+    Console.WriteLine($"The version is {version}");
+    Console.WriteLine($"##vso[task.setvariable variable=version;]{version}");
+}
+}
