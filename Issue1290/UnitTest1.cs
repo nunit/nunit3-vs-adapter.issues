0c12434298f1fb63f49d1c89e815802bdb9c7ebd
Terje Sandstrom <terje@hermit.no>
2025-08-11 13:17:44 +0200

    Added issue1290

---
+++ Issue1290/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿namespace Issue1290;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        string s = null!;
+        var l = s.Length; // Cause a nullref exception, should be seen in the output
+    }
+}
