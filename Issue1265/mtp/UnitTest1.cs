cb411d32e28605b3527df009eace5fca68942442
Terje Sandstrom <terje@hermit.no>
2025-08-16 20:37:32 +0200

    Added Issue1265  MTP

---
+++ Issue1265/mtp/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace mtp;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
