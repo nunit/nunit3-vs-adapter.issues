3b9f2e9a0b899eac6e8e2c14475caf920b8e74e9
Terje Sandstrom <terje@hermit.no>
2025-10-15 21:55:02 +0200

    updated engine to 3.20.1,

---
+++ NewEngine/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace NewEngine;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        int x = 42;
+        Assert.That(x, Is.EqualTo(42));
+    }
+}
