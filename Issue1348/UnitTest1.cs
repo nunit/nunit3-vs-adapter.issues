c34ff20fab1832bcc0822c7df1ff72efd759636b
Terje Sandstrom <terje@hermit.no>
2025-10-28 10:31:52 +0100

    Added issue 1348

---
+++ Issue1348/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using AwesomeAssertions;
+using NUnit.Framework;
+
+public sealed class ExceptionTests
+{
+    [Test]
+    public void TestMethod_AwesomeAssertions()
+    {
+        Action act = () => "foo".Should().BeNull();
+
+        act.Should().Throw<AssertionException>();
+    }
+}
