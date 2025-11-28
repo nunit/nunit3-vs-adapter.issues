d425734798a0e1103cc7cc0515568fc2651d4cde
Terje Sandstrom <terje@hermit.no>
2025-11-28 10:53:18 +0100

    Add Jason repro to Issue1152

---
+++ Issue1152/3-nunit-runner-jason/UnitTest1.cs
@@ -0,0 +1,27 @@
+namespace nunit_runner;
+
+public class Tests2
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test2()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test3()
+    {
+        Assert.Pass();
+    }
+}
