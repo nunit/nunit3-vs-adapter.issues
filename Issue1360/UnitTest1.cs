064527a696e1bfdd905a27a4676ab15d47a75eba
Terje Sandstrom <terje@hermit.no>
2025-12-03 18:55:34 +0100

    Added issue1360, deleted Issue1151 (no code)

---
+++ Issue1360/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Net472Issue
+{
+    public class UnitTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
