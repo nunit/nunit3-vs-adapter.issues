680e6578591aaf57ce8e80e83f56e0c8555c2fe9
Terje Sandstrom <terje@hermit.no>
2025-12-10 19:52:49 +0100

    added Issue1374

---
+++ Issue1374/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Issue1374;
+
+public class UnitTest1
+{
+    [Test]
+    public void TraceAssert_WithFalseCondition_ShouldThrowException()
+    {
+        var exception = Assert.Catch(() => System.Diagnostics.Trace.Assert(false));
+        Assert.That(exception, Is.Not.Null);
+    }
+}
