ad7c143279e6181d4202c3243f54a129295f33b8
Terje Sandstrom <terje@hermit.no>
2025-12-11 09:46:43 +0100

    Issue 1152 MTP updated

---
+++ console/Issue1796/AssemblyVersion/AssemblyVersion/TestFixture.cs
@@ -0,0 +1,16 @@
+﻿using CoreWCF.Configuration;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+public class TestFixture
+{
+    [Test]
+    public void Test()
+    {
+        MultipartReader xx = null!;
+        var services = new ServiceCollection();
+        // use Modules window to see that after this line older runtime libraries loaded
+        ServiceModelServiceCollectionExtensions.AddServiceModelServices(services);
+    }
+}
