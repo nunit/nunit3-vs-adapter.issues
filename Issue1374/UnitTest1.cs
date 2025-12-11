afc9f0c5732b1aaae3711e333bd726c7f4acf9a1
Terje Sandstrom <terje@hermit.no>
2025-12-11 15:37:40 +0100

    Issue 1374

--- Issue1374/UnitTest1.cs
+++ Issue1374/UnitTest1.cs
@@ -1,7 +1,20 @@
-﻿namespace Issue1374;
+﻿using System.Diagnostics;
+
+namespace Issue1374;
 
 public class UnitTest1
 {
+
+    [SetUp]
+    public void SetUp()
+    {
+        Trace.Listeners.Clear();
+        Trace.Listeners.Add(new DefaultTraceListener()
+        {
+            // AssertUiEnabled = false,
+        });
+    }
+
     [Test]
     public void TraceAssert_WithFalseCondition_ShouldThrowException()
     {
