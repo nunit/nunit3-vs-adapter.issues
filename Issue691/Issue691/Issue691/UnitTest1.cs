250e8f1539adfc3f7fc8768dfd6754024700eee2
Terje Sandstrom <terje@hermit.no>
2025-12-16 20:28:28 +0100

    Updated test logging

--- Issue691/Issue691/Issue691/UnitTest1.cs
+++ Issue691/Issue691/Issue691/UnitTest1.cs
@@ -13,9 +13,9 @@ namespace Issue691
         [TestCase("Works fine", "(\"")]
         [TestCase("Works fine", "()\"")]
         [TestCase("Works fine", "a b c")]
-       // [TestCase("Works fine", "\"")]
-       // [TestCase("Breaks test executor when using Run All", "\"()")]
-       // [TestCase("Breaks test executor when using Run All", "\")")]
+        [TestCase("Works fine", "\"")]
+        [TestCase("Breaks test executor when using Run All", "\"()")]
+        [TestCase("Breaks test executor when using Run All", "\")")]
         public void TestName(string expectedBehaviour, string data)
         {
             //Irrelevant
