b29b3acd8df076a7a21dfce3400e0707328aa508
Terje Sandstrom <terje@hermit.no>
2025-12-28 21:41:22 +0100

    Adding Issue1795

---
+++ Issue1795/V5_2/Tests.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.Loader;
+
+//All these work on V5.2
+[TestFixture]
+public class Tests
+{
+    string file = Path.GetFullPath("ClassLibrary.dll");
+
+    //fails on 6.0
+    [Test]
+    public void WithLoadFrom()
+    {
+        var assembly = Assembly.LoadFrom(file);
+        var type = assembly.GetType("Class1");
+        ClassicAssert.IsTrue(typeof(Class1) == type);
+    }
+
+    [Test]
+    public void WithGetAssemblyName()
+    {
+        var name = AssemblyName.GetAssemblyName(file);
+        var assembly = Assembly.Load(name);
+        var type = assembly.GetType("Class1");
+        ClassicAssert.IsTrue(typeof(Class1) == type);
+    }
+
+    [Test]
+    public void WithGetLoadContext()
+    {
+        var context = AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly())!;
+        var assembly = context.LoadFromAssemblyPath(file);
+        var type = assembly.GetType("Class1");
+        ClassicAssert.IsTrue(typeof(Class1) == type);
+    }
+
+    //fails on 6.0
+    [Test]
+    public void WithDefaultContext()
+    {
+        var context = AssemblyLoadContext.Default!;
+        var assembly = context.LoadFromAssemblyPath(file);
+        var type = assembly.GetType("Class1");
+        ClassicAssert.IsTrue(typeof(Class1) == type);
+    }
+}
