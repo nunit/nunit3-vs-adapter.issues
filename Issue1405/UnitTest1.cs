3e43091e474fa2515d6580e6b57e2912da320aa6
Terje Sandstrom <terje@hermit.no>
2026-01-15 14:32:20 +0100

    Added issue1405

---
+++ Issue1405/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+namespace NUnitIssue;
+
+public static class TestIssue
+{
+
+    [TestCase("This | That", false)]
+    public static void TestLength(string input, bool expected) =>
+        Assert.That(input.Length % 2 == 0, Is.EqualTo(expected));
+}
